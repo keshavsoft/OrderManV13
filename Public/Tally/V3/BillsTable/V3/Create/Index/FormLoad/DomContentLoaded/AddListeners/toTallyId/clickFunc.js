872eeaf3209d7477e4083152757331a6aede9237
keshavsoft <nknnkumar@yahoo.com>
2026-06-02 13:49:57 +0530

    red perfect

--- Public/Tally/V3/BillsTable/V3/Create/Index/FormLoad/DomContentLoaded/AddListeners/toTallyId/clickFunc.js
+++ Public/Tally/V3/BillsTable/V3/Create/Index/FormLoad/DomContentLoaded/AddListeners/toTallyId/clickFunc.js
@@ -1,24 +1,17 @@
 import { startFetchAsGet } from "./FetchAsGet/start.js";
 
 const clickFuncToRun = ({ inCurrentTarget }) => {
-    applyActive({ inCurrentTarget });
+    const localCurrentTarget = inCurrentTarget;
+    if (localCurrentTarget.classList.contains("danger")) {
+        alert("already posted to tally");
+
+        return;
+    };
+    // applyActive({ inCurrentTarget });
 
     const pkToPost = document.getElementById("htmlId").innerHTML;
 
     startFetchAsGet({ inPk: pkToPost });
-};
-
-const applyActive = ({ inCurrentTarget }) => {
-    const localCurrentTarget = inCurrentTarget;
-
-    const menu = localCurrentTarget.closest("#menu");
-
-    menu.querySelectorAll("li a.active")
-        .forEach(a => a.classList.remove("active"));
-
-    localCurrentTarget.classList.add("active");
-
-    localCurrentTarget.classList.add("active");
 };
 
 export { clickFuncToRun };
