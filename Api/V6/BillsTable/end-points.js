4c709fd27b429f2b17e08f22728987328fda3568
Keshav Nalam <nknnkumar@yahoo.com>
2026-06-02 10:04:52 +0530

    alter

--- Api/V6/BillsTable/end-points.js
+++ Api/V6/BillsTable/end-points.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import funcFromShowAll from './ShowAll/controller.js';
+import funcFromAlter from './Alter/controller.js';
 
 const tableName = "BillsTable";
 const tablePath = "Data/BillsTable.json";
@@ -8,5 +9,6 @@ const tablePath = "Data/BillsTable.json";
 const router = express.Router();
 
 router.get('/ShowAll', (req, res) => funcFromShowAll({ res, inTablePath: tablePath }));
+router.post('/Alter/:pk', express.json(), (req, res) => funcFromAlter({ req, res, inTablePath: tablePath }));
 
 export { router };
